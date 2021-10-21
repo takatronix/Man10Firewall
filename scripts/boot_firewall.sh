2110eb22365026e62f5b608e7370bdc577dcd6d6
takatronix <takatronix@gmail.com>
2021-10-21 16:13:50 +0900

    psuhing new information

--- scripts/boot_firewall.sh
+++ scripts/boot_firewall.sh
@@ -4,5 +4,5 @@ cd /home/man10/Man10Firewall/
 git pull
 python3 /home/man10/Man10Firewall/scripts/man10_firewall.py --threshold 50 --output /home/man10/Man10Firewall/iplist.txt --log /home/man10/mcserver/waterfall/logs/latest.log
 git add /home/man10/Man10Firewall/iplist.txt
-git commit
+git commit -m "psuhing new information"
 git push
