97e05d14977f85cb0a2ba75c59d2c6daee603951
Takashi Ohtsuka <takatronix@gmail.com>
2021-10-24 22:47:44 +0900

    Update update_ufw.sh

--- update_ufw.sh
+++ update_ufw.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-git checkout .
 git pull
-sh init.sh
 ufw default allow
 while read line;
     do ufw deny from $line to any;
+    echo $line
 done < iplist.txt
 ufw --force enable
 ufw reload
-ufw status verbose
 echo 終了
