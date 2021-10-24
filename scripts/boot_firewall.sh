925aca4adbe3a7cb1b65b3e20f5b6e2cb8712dce
takatronix <takatronix@gmail.com>
2021-10-24 23:22:08 +0900

    sh

--- scripts/boot_firewall.sh
+++ scripts/boot_firewall.sh
@@ -2,7 +2,7 @@
 screen -S man10 -X eval 'stuff "say DDoS攻撃元を調査しています...."\015'
 cd /home/man10/Man10Firewall/
 git pull
-python3 /home/man10/Man10Firewall/scripts/man10_firewall.py --threshold 50 --output /home/man10/Man10Firewall/iplist.txt --log /home/man10/mcserver/waterfall/logs/latest.log
+python3 /home/man10/Man10Firewall/scripts/man10_firewall.py --threshold 30 --output /home/man10/Man10Firewall/iplist.txt --log /home/man10/mcserver/waterfall/logs/latest.log
 git add /home/man10/Man10Firewall/iplist.txt
 git commit -m "pushing new information"
 git push
